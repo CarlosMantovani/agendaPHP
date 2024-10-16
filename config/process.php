68ec29526c480531519ea1ca194caf1994f71f4c
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-10-16 13:54:47 -0300

    criando query para mostrar detalhes do contato

--- config/process.php
+++ config/process.php
@@ -4,12 +4,32 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
+    $id;
     
-    $query = "SELECT * FROM contacts";
+    if (!empty($_GET)) {
+        $id = $_GET["id"];
+    }
+    
+    if (!empty($id)) {
+       $query = "SELECT * FROM contacts WHERE id = :id";
+       $stmt = $conn->prepare($query);
+        
+       $stmt->bindParam(":id", $id);
+       $stmt->execute();
+       $contact = $stmt-> fetch();
 
-    $stmt = $conn->prepare($query);
-    
-    $stmt->execute();
-    $contacts =  $stmt->fetchAll();
+    } else{
+        $contacts = [];
+
+      $query = "SELECT * FROM contacts";
+
+      $stmt = $conn->prepare($query);
+
+      $stmt->execute();
+      
+      $contacts = $stmt->fetchAll();
+    }
+
+
+   
 ?>
