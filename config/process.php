4fa6e3eb6217405e166608c14ce218905db52135
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-10-16 16:41:32 -0300

    criando query edit

--- config/process.php
+++ config/process.php
@@ -7,7 +7,7 @@
             $data=  $_POST;
             if (!empty($data)) {
                 
-                if($data["type"]=="create"){
+                if($data["type"]==="create"){
                     $name = $data["name"];
                     $phone = $data["phone"];
                     $observations= $data["observations"];
@@ -22,6 +22,28 @@
                     try {
                         $stmt->execute();
                         $_SESSION["msg"] = "Contato criado com sucesso!";
+                    
+                    } catch(PDOException $e) {
+                        $error = $e->getMessage();
+                        echo "Erro: $error";
+                    }
+                } else if($data["type"]=== "edit"){
+                    $name = $data["name"];
+                    $phone = $data["phone"];
+                    $observations= $data["observations"];
+                    $id = $data["id"];
+
+                    $query = "UPDATE contacts 
+                    SET name = :name, phone = :phone, observations = :observations WHERE id = :id";
+                    $stmt = $conn->prepare($query);
+                    $stmt->bindParam(":name", $name);
+                    $stmt->bindParam(":phone", $phone);
+                    $stmt->bindParam(":observations", $observations);
+                    $stmt->bindParam(":id", $id);
+                    
+                    try {
+                        $stmt->execute();
+                        $_SESSION["msg"] = "Contato atualizado com sucesso!";
                     
                     } catch(PDOException $e) {
                         $error = $e->getMessage();
