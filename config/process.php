b83fc379bef9ebc1249bbc4ffe8cc5f7e4d66d69
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-10-16 16:04:38 -0300

    criando query de cadastro de contato

--- config/process.php
+++ config/process.php
@@ -1,65 +1,65 @@
-<?php
-    session_start();
+        <?php
+            session_start();
 
-    include_once("connection.php");
-    include_once("url.php");
+            include_once("connection.php");
+            include_once("url.php");
 
-    $data=  $_POST;
-    if (!empty($data)) {
-        
-        if($data["type"]==="create"){
-            $name = $data["name"];
-            $phone = $data["phone"];
-            $observations= $data["observations"];
+            $data=  $_POST;
+            if (!empty($data)) {
+                
+                if($data["type"]=="create"){
+                    $name = $data["name"];
+                    $phone = $data["phone"];
+                    $observations= $data["observations"];
 
-            $query = "INSERT INTO contacts(name,phone,observations) VALUES (:name, :phone, :observations)";
+                    $query = "INSERT INTO contacts(name,phone,observations) VALUES (:name, :phone, :observations)";
 
-            $stmt = $conn->prepare($query);
-            $stmt->bindParam(":name", $name);
-            $stmt->bindParam(":phone", $phone);
-            $stmt->bindParam(":observations", $observations);
+                    $stmt = $conn->prepare($query);
+                    $stmt->bindParam(":name", $name);
+                    $stmt->bindParam(":phone", $phone);
+                    $stmt->bindParam(":observations", $observations);
 
-            try {
+                    try {
+                        $stmt->execute();
+                        $_SESSION["msg"] = "Contato criado com sucesso!";
+                    
+                    } catch(PDOException $e) {
+                        $error = $e->getMessage();
+                        echo "Erro: $error";
+                    }
+                }
+                header("Location:" . $BASE_URL . "../index.php");
+            }
+            else{
+                $id;
+            
+                if (!empty($_GET)) {
+                    $id = $_GET["id"];
+                }
+                
+                if (!empty($id)) {
+                $query = "SELECT * FROM contacts WHERE id = :id";
+                $stmt = $conn->prepare($query);
+                    
+                $stmt->bindParam(":id", $id);
                 $stmt->execute();
-                $_SESSION["msg"] = "Contato criado com sucesso!";
+                $contact = $stmt-> fetch();
             
-              } catch(PDOException $e) {
-                $error = $e->getMessage();
-                echo "Erro: $error";
-              }
-        }
-        header("Location:" . $BASE_URL . "../index.php");
-    }
-    else{
-        $id;
-    
-        if (!empty($_GET)) {
-            $id = $_GET["id"];
-        }
+                } else{
+                    $contacts = [];
+            
+                $query = "SELECT * FROM contacts";
+            
+                $stmt = $conn->prepare($query);
+            
+                $stmt->execute();
+                
+                $contacts = $stmt->fetchAll();
+                }
+            }
+
+            $conn = null;
         
-        if (!empty($id)) {
-           $query = "SELECT * FROM contacts WHERE id = :id";
-           $stmt = $conn->prepare($query);
-            
-           $stmt->bindParam(":id", $id);
-           $stmt->execute();
-           $contact = $stmt-> fetch();
-    
-        } else{
-            $contacts = [];
-    
-          $query = "SELECT * FROM contacts";
-    
-          $stmt = $conn->prepare($query);
-    
-          $stmt->execute();
-          
-          $contacts = $stmt->fetchAll();
-        }
-    }
 
-    $conn = null;
-   
-
-   
-?>
+        
+        ?>
